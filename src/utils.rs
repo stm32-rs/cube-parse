43b15575cc772432363a4f8df5a1cd904397c969
HarkonenBade <github@harkonen.net>
2019-01-02 23:27:05 +0000

    Buffer input files to improve performance slightly

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 use std::{
     error::Error,
+    io::BufReader,
     fs::File,
     path::Path,
 };
@@ -8,7 +9,7 @@ use serde::Deserialize;
 
 pub fn load_file<'a, P: AsRef<Path>, Q: AsRef<Path>, R: Deserialize<'a>>(db_dir: P, file_path: Q) -> Result<R, Box<Error>> {
     let db_dir = db_dir.as_ref();
-    let mut fin = File::open(&db_dir.join(file_path.as_ref()))?;
+    let mut fin = BufReader::new(File::open(&db_dir.join(file_path.as_ref()))?);
 
     Ok(serde_xml_rs::deserialize(&mut fin)?)
 }
