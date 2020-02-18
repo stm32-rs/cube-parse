059b66d03ca37cbec4aa384126e1933390fc93c5
Danilo Bargen <mail@dbrgn.ch>
2020-02-19 00:19:25 +0100

    family: Add some code documentation

--- src/family.rs
+++ src/family.rs
@@ -12,6 +12,7 @@ pub struct Families {
     families: Vec<Family>,
 }
 
+/// A MCU family (e.g. "STM32F0" or "STM32L3").
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Family {
@@ -20,20 +21,48 @@ pub struct Family {
     sub_families: Vec<SubFamily>,
 }
 
+/// A MCU subfamily (e.g. "STM32F0x0 Value Line").
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct SubFamily {
     pub name: String,
     #[serde(rename = "Mcu")]
-    mcus: Vec<Mcu>,
+    pub mcus: Vec<Mcu>,
 }
 
+/// A MCU (e.g. STM32L071KBTx).
+///
+/// Note that multiple MCUs (with unique `ref_name`) share a common name. For
+/// example:
+///
+/// - `<Mcu Name="STM32L071K(B-Z)Tx" PackageName="LQFP32" RefName="STM32L071KBTx" RPN="STM32L071KB">`
+/// - `<Mcu Name="STM32L071K(B-Z)Tx" PackageName="LQFP32" RefName="STM32L071KZTx" RPN="STM32L071KZ">`
+///
+/// Both MCUs share the same name, but the ref name is different.
+///
+/// The meaning of the name, using the `STM32L071KBTx` as an example:
+///
+/// |Part |Meaning |
+/// |-----|--------|
+/// |STM32|Family  |
+/// | L   |Type    |
+/// | 0   |Core    |
+/// | 71  |Line    |
+/// | K   |Pincount|
+/// | B   |Flash   |
+/// | T   |Package |
+/// | x   |Temp    |
+/// |-----|--------|
+///
+/// See https://ziutek.github.io/2018/05/07/stm32_naming_scheme.html for more details.
+///
+/// Note that sometimes there are exceptions from this naming rule.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Mcu {
     pub name: String,
     package_name: String,
-    ref_name: String,
+    pub ref_name: String,
 }
 
 impl Families {
